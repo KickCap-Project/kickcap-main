fce1fd00de212a93ce7d47dffc73b50e4d4c8650
JinY <wlsdud6221@naver.com>
2024-09-12 17:53:21 +0900

    [FE] feat : 상태관리 추가

--- kickcap-police/src/pages/Reported/ReportListPage.jsx
+++ kickcap-police/src/pages/Reported/ReportListPage.jsx
@@ -13,37 +13,12 @@ const s = {
     overflow-y: auto;
   `,
   mainArea: styled.main``,
-  searchArea: styled.div`
-    width: 430px;
-    height: 50px;
-    display: flex;
-    justify-content: space-around;
-    background-color: ${(props) => props.theme.bgColor};
-    align-items: center;
-    border-radius: 10px;
-    box-shadow: rgba(0, 0, 0, 0.1) 0px 5px 2px, rgba(0, 0, 0, 0.3) 0px 3px 3px;
-    position: relative;
-    margin: 0 auto;
-    top: -20px;
-    z-index: 100;
-  `,
-  searchInput: styled.input`
-    width: 350px;
-    height: 50px;
-    &::placeholder {
-      color: ${(props) => props.theme.placeholderColor};
-    }
-  `,
 };
 
 const ReportListPage = () => {
   return (
     <s.Container>
-      <Header title={'이 의 제 기'} subTitle={'단속 사항에 대한 문의 내역입니다.'} />
-      <s.searchArea>
-        <s.searchInput placeholder="작성자를 입력하세요" />
-        <IconSvg Ico={search} width={'20px'} cursor={'pointer'} />
-      </s.searchArea>
+      <Header title={'국민 신고함'} subTitle={'국민들의 자발적 킥보드 신고 내역입니다.'} />
       <s.mainArea>
         <ReportList />
       </s.mainArea>
