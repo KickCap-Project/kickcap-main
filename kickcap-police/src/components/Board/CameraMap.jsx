d64b76b3d5b252433a5eb9f627849ab0b9e34b85
JinY <wlsdud6221@naver.com>
2024-09-18 18:58:42 +0900

    [FE] fix : data 미존재시 처리

--- kickcap-police/src/components/Board/CameraMap.jsx
+++ kickcap-police/src/components/Board/CameraMap.jsx
@@ -9,7 +9,7 @@ import cam1 from '../../asset/cam1.png';
 import cam2 from '../../asset/cam2.png';
 import cam3 from '../../asset/cam3.png';
 import cam4 from '../../asset/cam4.png';
-import { useLocation, useParams } from 'react-router';
+import { useLocation, useNavigate, useParams } from 'react-router';
 
 const s = {
   Container: styled.div`
@@ -21,13 +21,18 @@ const { kakao } = window;
 
 const CameraMap = ({ point }) => {
   const location = useParams();
-  const data = useLocation().state.data; //data 미존재시 => 직접 url입력상황이면 에러페이지 이동하는ㄴ거 하기
+  const navigate = useNavigate();
+  const data = useLocation().state?.data || null; //data 미존재시 => 직접 url입력상황이면 에러페이지 이동하는ㄴ거 하기
   console.log(location);
   console.log(data);
   const [map, setMap] = useState(null);
 
   //처음 지도 그리기
   useEffect(() => {
+    if (data === null) {
+      navigate('*');
+      return;
+    }
     const container = document.getElementById('map');
     const options = { center: new kakao.maps.LatLng(data.lat, data.lng) };
     const kakaoMap = new kakao.maps.Map(container, options);
