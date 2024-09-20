4e33b05aeb71eeadc42c321bf719582f0d740c3a
1139091SSAFY <iiky2124@gmail.com>
2024-09-20 14:25:11 +0900

    [FE] feat: 신고 기능 페이지 개발 중
    Former-commit-id: 78eda79836cd5d3e707bee5868963b2d6b9e859e

---
+++ kickcap-user/src/components/Report/ReportIllegalParkingForm.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+import Button from '../Common/Button';
+import Input from '../Common/Input';
+import TextArea from '../Common/TextArea';
+
+import UploadImgButton from '../../components/Report/UploadImgButtom';
+
+const s = {
+  Form: styled.form`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `,
+  ImageArea: styled.div`
+    width: 100%;
+    height: fit-content;
+  `,
+  InputArea: styled.div`
+    width: 100%;
+    height: fit-content;
+    display: flex;
+    justify-content: space-between;
+  `,
+};
+
+const ReportIllegalParkingForm = () => {
+  const [image, setImage] = useState('');
+  const [kickboardNumber, setKickboardNumber] = useState("'");
+  const [description, setDescription] = useState('');
+
+  return (
+    <s.Form>
+      {image ? <></> : <UploadImgButton />}
+      <s.InputArea>
+        <Input
+          id={'kickboardNumber'}
+          name={'kickboardNumber'}
+          width={'33%'}
+          height={'40px'}
+          bold={'500'}
+          size={'12px'}
+          placeholder={'킥보드 번호판 입력'}
+          text-align={'center'}
+        />
+        <Input
+          mode={'read'}
+          id={'image'}
+          name={'image'}
+          width={'66%'}
+          height={'40px'}
+          bold={'500'}
+          size={'12px'}
+          InputColor="AreaColor"
+          placeholder={'사진 첨부 시 정보가 입력됩니다.'}
+        >
+          {image}
+        </Input>
+      </s.InputArea>
+      <TextArea
+        id={'desciption'}
+        name={'description'}
+        width={'100%'}
+        height={'35vh'}
+        bold={'500'}
+        size={'15px'}
+        placeholder={'내용을 입력하세요.'}
+        margin={'10px'}
+      />
+      {image && kickboardNumber && description ? (
+        <Button width={'90%'} height={'40px'} bold={'700'} size={'24px'}>
+          작성 완료
+        </Button>
+      ) : (
+        <Button type={'sub'} width={'90%'} height={'40px'} bold={'700'} size={'24px'}>
+          작성 완료
+        </Button>
+      )}
+    </s.Form>
+  );
+};
+
+export default ReportIllegalParkingForm;
---
+++ kickcap-user/src/components/Report/UploadImgButtom.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import camera_upload from '../../asset/img/svg/camera_upload.svg';
+
+const s = {
+  Button: styled.div`
+    width: 100%;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 1vh;
+    margin-bottom: 1vh;
+    padding-top: 4vh;
+    padding-bottom: 4vh;
+    border: 1px solid black;
+  `,
+  ImgContainer: styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  `,
+  ImgBox: styled.img``,
+  Description: styled.div`
+    width: 100%;
+    text-align: center;
+    color: ${(props) => props.theme.mainColor};
+    word-break: keep-all;
+    overflow-wrap: break-word;
+  `,
+};
+
+const UploadImgButton = () => {
+  const description = '킥보드 번호판이 나온 전체 상황 사진을 첨부해주세요.';
+  return (
+    <s.Button>
+      <s.ImgContainer>
+        <s.ImgBox src={camera_upload} />
+      </s.ImgContainer>
+      <s.Description>{description}</s.Description>
+    </s.Button>
+  );
+};
+
+export default UploadImgButton;
