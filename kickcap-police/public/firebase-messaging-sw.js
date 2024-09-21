3b18ddefb762dbf03f0fd87a38d5f9f52b5988ca
JinY <wlsdud6221@naver.com>
2024-09-22 00:01:33 +0900

    [FE] feat : FCM token 추가
    Former-commit-id: 70b4eab58b7196259aa5a1325f6490a7752c9c1e

---
+++ kickcap-police/public/firebase-messaging-sw.js
@@ -0,0 +1,33 @@
+self.addEventListener("install", function (e) {
+  console.log("fcm sw install..");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (e) {
+  console.log("fcm sw activate..");
+});
+
+self.addEventListener("push", function (e) {
+  console.log("push: ", e.data.json());
+  if (!e.data.json()) return;
+
+  const notification = e.data.json().notification;
+  let data = e.data.json().data;
+  const notificationTitle = notification.title;
+  const notificationOptions = {
+    body: notification.body,
+    icon: "/logo192.png",
+    data: data.url,
+  };
+
+  e.waitUntil(
+    self.registration.showNotification(notificationTitle, notificationOptions)
+  );
+});
+
+self.addEventListener("notificationclick", function (event) {
+  console.log("notification click");
+  event.notification.close();
+  let url = event.notification.data;
+  clients.openWindow(url);
+});
