1f4088ce46a6861a96ed962131dc1650a57f755b
minji <minipeach0923@gmail.com>
2024-09-25 13:53:52 +0900

    [BE] feat : dashboard 구현 중
    dto 작성, 신고로직 끝난 뒤 서비스 구현 예정

---
+++ kickcap-BE/src/main/java/com/ssafy/kickcap/dashboard/dto/DayToalResponse.java
@@ -0,0 +1,8 @@
+package com.ssafy.kickcap.dashboard.dto;
+
+public class DayToalResponse {
+    private int crackDown;   // 총 단속 수
+    private int report;      // 총 신고 수
+    private int accident;    // 총 사고 수
+    private String date;     // 날짜 (YY-DD)
+}
---
+++ kickcap-BE/src/main/java/com/ssafy/kickcap/dashboard/dto/TimeCrackdownResponse.java
@@ -0,0 +1,6 @@
+package com.ssafy.kickcap.dashboard.dto;
+
+public class TimeCrackdownResponse {
+    private int crackDown;   // 총 단속 수
+    private int timeIndex;   // 시간 인덱스
+}
