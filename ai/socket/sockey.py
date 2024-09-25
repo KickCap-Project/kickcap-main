e884e5f6cb1ea0f7f97785c2c6f8de827731238a
김종원 <roista57@gmail.com>
2024-09-25 16:01:08 +0900

    [AI] fix: socket 수정

--- ai/socket/sockey.py
+++ ai/socket/sockey.py
@@ -1,27 +1,17 @@
 import asyncio
-from aiohttp import web
+from aiohttp import web, ClientSession
 import cv2
 import numpy as np
 import aiohttp
-from ultralytics import YOLO
+import datetime
 import os
-import datetime  # datetime module added
-import requests
+from dotenv import load_dotenv
 
-# Load the YOLO model. Ultralytics will download the model if it doesn't exist.
-# model = YOLO('runs/detect/train5/weights/best.pt')  # yolov8n.pt is a smaller, lightweight version
-model = YOLO('best.pt')
+# .env 파일에서 환경 변수 로드
+load_dotenv()
 
-
-# 파일을 서버로 업로드하는 함수
-def upload_image(image_buffer, file_name):
-    url = "https://j11b102.p.ssafy.io/image/upload/camera"  # Postman에서 사용한 URL
-    # 메모리 버퍼를 바이너리로 전송
-    files = {'image': (f'{file_name}', image_buffer.tobytes(), 'image/jpeg')}
-    response = requests.post(url, files=files)  # POST 요청 보내기
-    print(response.status_code)  # 응답 코드 출력
-    print(response.text)  # 서버 응답 내용 출력
-
+# 환경 변수에서 DB 연결 정보 가져오기
+API_ENDPOINT = os.getenv("API_ENDPOINT")
 
 async def video_stream(request):
     ws = web.WebSocketResponse()
@@ -31,38 +21,44 @@ async def video_stream(request):
     frame_interval = 0.3
     last_frame_time = datetime.datetime.now()
 
-    directories = ['./Scooter_No_Helmet', './Scooter_Two']
-    for directory in directories:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
+    async with ClientSession() as session:
+        while True:
+            try:
+                # 웹소켓으로부터 프레임 데이터 수신
+                frame = await ws.receive_bytes()
+                # 넘파이 배열로 변환
+                nparr = np.frombuffer(frame, np.uint8)
+                # 이미지를 디코딩
+                image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    while True:
-        try:
-            # Receive the frame data from WebSocket
-            frame = await ws.receive_bytes()
-            # Convert frame data to numpy array
-            nparr = np.frombuffer(frame, np.uint8)
-            # Decode image from numpy array
-            image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+                if image is not None:
+                    current_time = datetime.datetime.now()
+                    time_elapsed = (current_time - last_frame_time).total_seconds()
 
-            if image is not None:
-                current_time = datetime.datetime.now()
-                time_elapsed = (current_time - last_frame_time).total_seconds()
+                    if time_elapsed >= frame_interval:
+                        # 이미지를 메모리 버퍼로 인코딩 (JPEG 포맷으로 인코딩)
+                        _, image_encoded = cv2.imencode('.jpg', image)
+                        # FastAPI 엔드포인트로 이미지 전송
+                        files = {'image_file': image_encoded.tobytes()}
+                        async with session.post(API_ENDPOINT, data=files) as resp:
+                            if resp.status == 200:
+                                # 응답으로 받은 주석 처리된 이미지 수신
+                                annotated_image_bytes = await resp.read()
+                                # 클라이언트로 전송
+                                await ws.send_bytes(annotated_image_bytes)
+                            else:
+                                print(f"Error from FastAPI endpoint: {resp.status}")
+                        # 마지막 프레임 시간을 현재 시간으로 업데이트
+                        last_frame_time = datetime.datetime.now()
 
-                if time_elapsed >= frame_interval:
-                    
-
-                    # Update last_frame_time to current
-                    last_frame_time = datetime.datetime.now()
-
-        except (aiohttp.WSServerHandshakeError, aiohttp.ClientError) as e:
-            print(f"Error: {e}")
-            break
+            except (aiohttp.WSServerHandshakeError, aiohttp.ClientError) as e:
+                print(f"Error: {e}")
+                break
     print("Client disconnected")
     return ws
 
 app = web.Application()
 app.router.add_get('/video', video_stream)
 
-# Run the app
+# 앱 실행
 web.run_app(app, port=8765)
