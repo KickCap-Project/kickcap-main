455b04467d8802da43b24bc86d23de8533f52afe
minji <minipeach0923@gmail.com>
2024-09-25 17:44:22 +0900

    [BE] feat : 대시보드 전국 일주일 통계 구현 중
    전국, 시도, 구군 코드 통일 필요

--- kickcap-BE/src/main/java/com/ssafy/kickcap/dashboard/controller/DashboardController.java
+++ kickcap-BE/src/main/java/com/ssafy/kickcap/dashboard/controller/DashboardController.java
@@ -1,27 +1,37 @@
-package com.ssafy.kickcap.dashboard.controller;
-
-import com.ssafy.kickcap.dashboard.dto.WeekResponse;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RequiredArgsConstructor
-@RestController
-@RequestMapping("/dashboard")
-@Tag(name = "Dashboard API", description = "경찰 대시보드 관련 API")
-public class DashboardController {
-
-
-
+//package com.ssafy.kickcap.dashboard.controller;
+//
+//import com.ssafy.kickcap.cctv.repository.CrackdownRepository;
+//import com.ssafy.kickcap.dashboard.dto.WeekResponse;
+//import com.ssafy.kickcap.dashboard.service.DashboardService;
+//import com.ssafy.kickcap.report.repository.ReportRepository;
+//import io.swagger.v3.oas.annotations.Operation;
+//import io.swagger.v3.oas.annotations.tags.Tag;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RequiredArgsConstructor
+//@RestController
+//@RequestMapping("/dashboard")
+//@Tag(name = "Dashboard API", description = "경찰 대시보드 관련 API")
+//public class DashboardController {
+//
+//    private final DashboardService dashboardService;
+//
 //    @GetMapping("")
 //    @Operation(summary = "전국 1주일 통계 데이터 조회", description = "전국의 모든 카테고리의 1주일 통계 데이터를 조회")
 //    public ResponseEntity<WeekResponse> NationwideWeekData() {
-//
+//        WeekResponse weekResponse = dashboardService.searchNationwideWeekData(0,0);
+//        return ResponseEntity.ok().body(weekResponse);
 //    }
-
-
-}
+//
+//    @GetMapping("/regions")
+//    @Operation(summary = "전국 1주일 통계 데이터 조회", description = "전국의 모든 카테고리의 1주일 통계 데이터를 조회")
+//    public ResponseEntity<WeekResponse> gugunWeekData(@RequestParam int regionsId, @RequestParam int gugunId){
+//        WeekResponse weekResponse = dashboardService.searchGugunWeekData(regionsId, gugunId);
+//        return ResponseEntity.ok().body(weekResponse);
+//    }
+//}
--- kickcap-BE/src/main/java/com/ssafy/kickcap/report/repository/ReportRepository.java
+++ kickcap-BE/src/main/java/com/ssafy/kickcap/report/repository/ReportRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
@@ -17,7 +18,7 @@ public interface ReportRepository extends JpaRepository<Report, Long> {
     long countByViolationTypeAndApproveStatusAndPolice(ViolationType violationType, ApproveStatus approveStatus, Police police);
 
     // 완료된 건(APPROVED, REJECTED 상태)의 총 개수를 조회하는 쿼리 메서드
-    @Query("SELECT count(r) FROM Report  r WHERE r.violationType = :violationType AND r.approveStatus IN :statuses AND r.police = :police ")
+    @Query("SELECT COUNT(r) FROM Report  r WHERE r.violationType = :violationType AND r.approveStatus IN :statuses AND r.police = :police ")
     long countCompletedReports(@Param("violationType") ViolationType violationType, @Param("statuses") List<ApproveStatus> statuses, @Param("police") Police police);
 
     // 주어진 위반 유형과 승인 상태가 'UNAPPROVED'인 리포트를 페이지네이션하여 조회하는 쿼리
@@ -26,4 +27,9 @@ public interface ReportRepository extends JpaRepository<Report, Long> {
     // 주어진 위반 유형과 승인 상태가 'REJECTED, APPROVED'인 리포트를 페이지네이션하여 조회하는 쿼리
     Page<Report> findByViolationTypeAndApproveStatusInAndPoliceOrderByIdDesc(ViolationType violationType, List<ApproveStatus> statuses, Police police, Pageable pageable);
 
+    @Query("SELECT COUNT(r) FROM Report r WHERE r.reportTime BETWEEN :startDate AND :endDate GROUP BY DATE(r.reportTime)")
+    List<Long> countReportsByDateRange(ZonedDateTime startDate, ZonedDateTime endDate);
+
+    @Query("SELECT COUNT(a) FROM AccidentReport a WHERE a.reportTime BETWEEN :startDate AND :endDate GROUP BY DATE(a.reportTime)")
+    List<Long> countAccidentsByDateRange(ZonedDateTime startDate, ZonedDateTime endDate);
 }
