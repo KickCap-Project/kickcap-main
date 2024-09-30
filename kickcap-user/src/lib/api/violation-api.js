bccf66a676e45579001705a584323bf63126ce57
1139091SSAFY <iiky2124@gmail.com>
2024-09-30 09:35:24 +0900

    [FE] fix: 고지서 내역 이미지 표시 수정
    Former-commit-id: 0756c2443c80820b28a20d8071b9e0ac65620d98

--- kickcap-user/src/lib/api/violation-api.js
+++ kickcap-user/src/lib/api/violation-api.js
@@ -24,22 +24,6 @@ export const getBillList = async (pageNo) => {
 };
 
 export const getBillDetail = async (billId) => {
-  const dummy = {
-    idx: billId,
-    kickboardNumber: 'G2468',
-    date: '2024-09-27T11:03:00Z',
-    address: '더미',
-    violationType: '안전모 미착용',
-    demerit: 3,
-    fine: 20000,
-    totalBill: 30000,
-    deadLine: '2024-09-30T14:59:59Z',
-    police: '더미',
-    isFlag: 'UNPAID',
-    isObjection: 0,
-    imgSrc: '',
-  };
-
   try {
     const response = await axiosInstance.get(`/bills/${billId}`);
 
@@ -49,28 +33,27 @@ export const getBillDetail = async (billId) => {
       return response.data;
     } else {
       console.log('고지서 상세 정보를 받아오는 데 문제가 발생했습니다. 잠시 후 다시 시도해주세요.');
-      return dummy;
-      // alert('고지서 상세 정보를 받아오는 데 문제가 발생했습니다. 잠시 후 다시 시도해주세요.');
-      // navigate
+      alert('고지서 상세 정보를 받아오는 데 문제가 발생했습니다. 잠시 후 다시 시도해주세요.');
+      return;
     }
   } catch {
     console.log('고지서 상세 정보를 받아오는 데 실패했습니다. 잠시 후 다시 시도해주세요.');
-    return dummy;
-    // alert('고지서 상세 정보를 받아오는 데 실패했습니다. 잠시 후 다시 시도해주세요.');
+    alert('고지서 상세 정보를 받아오는 데 실패했습니다. 잠시 후 다시 시도해주세요.');
+    return;
   }
 };
 
-export const getImgFile = async (imgSrc) => {
+export const getImgFile = async (imageSrc) => {
   try {
-    const response = await axios.get(imgSrc, { responseType: 'blob' });
+    const response = await axios.get(imageSrc, { responseType: 'blob' });
 
     if (response.status === 200) {
       console.log('이미지 파일을 불러오는 데 성공했습니다.');
       return response.data;
+    } else {
+      console.log(`이미지를 불러오는 데 실패했습니다: ${response.status}`);
+      return null;
     }
-
-    console.log(`이미지를 불러오는 데 실패했습니다: ${response.status}`);
-    return null;
   } catch (err) {
     console.log(`이미지를 불러오는 중 에러가 발생했습니다: ${err}`);
     return null;
@@ -90,5 +73,5 @@ export const submitObjection = async (billId, title, content) => {
     }
   } catch (err) {
     console.log(`이의제기 POST 요청 실패: ${err}`);
-  } 
+  }
 };
