bf12b484eacc44ff98728a86edef059753476e02
유현진 <a92046503@gmail.com>
2024-10-03 00:28:17 +0900

    [BE] feat:  알림 목록 조회 api 완료
    목록조회 끝

--- kickcap-BE/src/main/java/com/ssafy/kickcap/notification/controller/NotificationController.java
+++ kickcap-BE/src/main/java/com/ssafy/kickcap/notification/controller/NotificationController.java
@@ -1,10 +1,20 @@
 package com.ssafy.kickcap.notification.controller;
 
+import com.ssafy.kickcap.config.oauth.CustomOAuth2User;
+import com.ssafy.kickcap.notification.dto.NotificationListResponseDto;
+import com.ssafy.kickcap.notification.service.NotificationService;
+import com.ssafy.kickcap.report.dto.ReportListResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -12,4 +22,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "알림 컨트롤러", description = "알림 관련 API")
 @RequestMapping("/notifications")
 public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    @GetMapping()
+    @Operation(summary = "알림 목록 조회")
+    public ResponseEntity<List<NotificationListResponseDto>> getNotifications(@AuthenticationPrincipal CustomOAuth2User principal) {
+        List<NotificationListResponseDto> notifications = notificationService.getNotifications(principal.getId());
+        return ResponseEntity.ok(notifications);
+    }
 }
--- kickcap-BE/src/main/java/com/ssafy/kickcap/notification/repository/NotificationRepository.java
+++ kickcap-BE/src/main/java/com/ssafy/kickcap/notification/repository/NotificationRepository.java
@@ -1,7 +1,15 @@
 package com.ssafy.kickcap.notification.repository;
 
 import com.ssafy.kickcap.notification.entity.Notification;
+import com.ssafy.kickcap.user.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.ZonedDateTime;
+import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    @Query("SELECT n FROM Notification n WHERE n.member = :member AND n.isRead <> 'Y' OR (n.isRead = 'Y' AND n.createdAt >= :sevenDaysAgo) ORDER BY n.createdAt DESC")
+    List<Notification> findNotificationsExcludingOldRead(Member member, ZonedDateTime sevenDaysAgo);
 }
