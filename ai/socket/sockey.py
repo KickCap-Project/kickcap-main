f9b2d2fa656c08ef2691fd4005e2c9b6b0e555f4
김종원 <roista57@gmail.com>
2024-10-07 17:21:33 +0900

    [AI] fix: Websocket 브로드캐스트 수정

--- ai/socket/sockey.py
+++ ai/socket/sockey.py
@@ -13,6 +13,18 @@ load_dotenv()
 API_ENDPOINT = os.getenv("API_ENDPOINT")
 
 connected_clients = set()
+image_queue = asyncio.Queue()
+
+
+async def broadcast_images():
+    while True:
+        frame = await image_queue.get()  # Queue에서 데이터를 가져옴 (대기)
+        for client in connected_clients:
+            try:
+                await client.send_bytes(frame)  # 브로드캐스트
+            except Exception as e:
+                print(f"Error broadcasting to client: {e}")
+        image_queue.task_done()  # Queue에서 작업이 완료되었음을 알림
 
 
 async def video_stream(request):
@@ -33,10 +45,8 @@ async def video_stream(request):
                     # 웹소켓으로부터 프레임 데이터 수신
                     frame = msg.data
 
-                    # 원본 프레임을 다른 클라이언트에게 브로드캐스트
-                    for client in connected_clients:
-                        if client != ws:
-                            await client.send_bytes(frame)
+                    # 원본 프레임을 Queue에 넣음
+                    await image_queue.put(frame)
 
                     # 넘파이 배열로 변환
                     nparr = np.frombuffer(frame, np.uint8)
@@ -51,14 +61,12 @@ async def video_stream(request):
                         async with session.post(API_ENDPOINT, data=files) as resp:
                             if resp.status != 200:
                                 print(f"Error from FastAPI endpoint: {resp.status}")
-                        # 주석 처리된 이미지를 다른 클라이언트들에게 브로드캐스트
 
+                        # 주석 처리된 이미지를 다른 클라이언트들에게 브로드캐스트 (Queue에 넣음)
                         resized_image = cv2.resize(image, (640, 360))
                         _, resize_image_encoded = cv2.imencode('.jpg', resized_image)
                         annotated_image_bytes = resize_image_encoded.tobytes()
-                        for client in connected_clients:
-                            if client != ws:
-                                await client.send_bytes(annotated_image_bytes)
+                        await image_queue.put(annotated_image_bytes)
                 elif msg.type == aiohttp.WSMsgType.ERROR:
                     print('ws connection closed with exception %s' % ws.exception())
         except Exception as e:
@@ -71,6 +79,10 @@ async def video_stream(request):
 
 app = web.Application()
 app.router.add_get('/video', video_stream)
+
+# 브로드캐스트 작업을 별도의 비동기 태스크로 실행
+loop = asyncio.get_event_loop()
+loop.create_task(broadcast_images())
 
 # 앱 실행
 web.run_app(app, port=8765)
