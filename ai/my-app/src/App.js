e536729f30183c4c57466829728ffa55fa4c8016
김종원 <roista57@gmail.com>
2024-10-07 15:14:22 +0900

    [AI] fix: Socket Broadcast 수정

--- ai/my-app/src/App.js
+++ ai/my-app/src/App.js
@@ -161,69 +161,73 @@ const VideoStream = () => {
 
     useEffect(() => {
         // WebSocket 서버에 연결
-        socketRef.current = new WebSocket("wss://j11b102.p.ssafy.io/cctv/video");
-
+        socketRef.current = new WebSocket('wss://j11b102.p.ssafy.io/cctv/video');
+    
         // 유저의 미디어 스트림을 가져오기
         navigator.mediaDevices
-            .getUserMedia({
-                video: {
-                    width: { ideal: 1920 },
-                    height: { ideal: 1080 },
-                    facingMode: "environment",
-                },
-            })
-            .then((stream) => {
-                const videoElement = videoRef.current;
-                videoElement.srcObject = stream;
-
-                videoElement.onloadedmetadata = () => {
-                    videoElement.play();
-
-                    const videoTrack = stream.getVideoTracks()[0];
-                    const imageCapture = new ImageCapture(videoTrack);
-
-                    const sendFrame = () => {
-                        imageCapture
-                            .grabFrame()
-                            .then((imageBitmap) => {
-                                const canvas = document.createElement("canvas");
-                                canvas.width = 1920;
-                                canvas.height = 1080;
-                                const context = canvas.getContext("2d");
-                                context.drawImage(imageBitmap, 0, 0, canvas.width, canvas.height);
-                                canvas.toBlob((blob) => {
-                                    socketRef.current.send(blob); // 프레임 전송
-                                }, "image/jpeg");
-                            })
-                            .catch((error) => {
-                                console.error("프레임 캡처 중 오류 발생:", error);
-                            });
-                    };
-                    setInterval(sendFrame, 333); // 3 FPS로 프레임 전송
-                };
-            })
-            .catch((error) => {
-                console.error("웹캠 접근 오류:", error);
-            });
-
-        // 현재 기기가 모바일이 아닌 경우에만 WebSocket으로 수신한 데이터를 처리
-        const isMobile = /Mobi|Android/i.test(navigator.userAgent);
-
-        if (!isMobile) {
-            // WebSocket을 통해 수신한 데이터 처리 (모바일이 아닌 경우에만)
-            socketRef.current.onmessage = (event) => {
-                const imageBlob = event.data;
-                const imageUrl = URL.createObjectURL(imageBlob);
-                setAnnotatedImage(imageUrl); // 수신한 이미지 URL을 상태에 저장
+          .getUserMedia({
+            video: {
+              width: { ideal: 1920 },
+              height: { ideal: 1080 },
+              facingMode: "environment",
+            },
+          })
+          .then((stream) => {
+            const videoElement = videoRef.current;
+            videoElement.srcObject = stream;
+    
+            videoElement.onloadedmetadata = () => {
+              videoElement.play();
+    
+              const videoTrack = stream.getVideoTracks()[0];
+              const imageCapture = new ImageCapture(videoTrack);
+    
+              const sendFrame = () => {
+                imageCapture
+                  .grabFrame()
+                  .then((imageBitmap) => {
+                    const canvas = document.createElement('canvas');
+                    canvas.width = 1920;
+                    canvas.height = 1080;
+                    const context = canvas.getContext('2d');
+                    context.drawImage(imageBitmap, 0, 0, canvas.width, canvas.height);
+                    canvas.toBlob(
+                      (blob) => {
+                        socketRef.current.send(blob); // 프레임 전송
+                      },
+                      'image/jpeg'
+                    );
+                  })
+                  .catch((error) => {
+                    console.error('프레임 캡처 중 오류 발생:', error);
+                  });
+              };
+              setInterval(sendFrame, 333); // 3 FPS로 프레임 전송
             };
-        }
-
-        return () => {
-            if (socketRef.current) {
-                socketRef.current.close();
-            }
+          })
+          .catch((error) => {
+            console.error('웹캠 접근 오류:', error);
+          });
+    
+         // 현재 기기가 모바일이 아닌 경우에만 WebSocket으로 수신한 데이터를 처리
+      const isMobile = /Mobi|Android/i.test(navigator.userAgent);
+      
+      if (!isMobile) {
+        // WebSocket을 통해 수신한 데이터 처리 (모바일이 아닌 경우에만)
+        socketRef.current.onmessage = (event) => {
+          const imageBlob = event.data;
+          const imageUrl = URL.createObjectURL(imageBlob);
+          setAnnotatedImage(imageUrl); // 수신한 이미지 URL을 상태에 저장
         };
-    }, []);
+      }
+    
+        return () => {
+          if (socketRef.current) {
+            socketRef.current.close();
+          }
+        };
+      }, []);
+    
 
     return (
         <div>
