e52aec67adebcc8d68f9e8b7c51dc3d7325935b5
JinY <wlsdud6221@naver.com>
2024-10-10 03:19:34 +0900

    [FE] feat : 사고발생시 음성 추가
    Former-commit-id: f0019cfb3a46047cc37b1481d302bb44de5b2cc3

--- kickcap-police/src/pages/EmergencyFramePage.jsx
+++ kickcap-police/src/pages/EmergencyFramePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import { useQuery } from '@tanstack/react-query';
 import BoardEmergencyModal from '../components/Modal/BoardEmergencyModal';
@@ -6,6 +6,7 @@ import { modalActions, selectIsEmergency } from '../store/modal';
 import { useAppDispatch, useAppSelector } from '../lib/hook/useReduxHook';
 import { getEmergency } from '../lib/api/main-api';
 import { Outlet } from 'react-router';
+import sound from '../asset/accidentSound.mp3';
 
 const s = {
   Container: styled.div`
@@ -17,6 +18,7 @@ const s = {
   `,
 };
 const EmergencyPage = () => {
+  const audioRef = useRef(null); // Create a ref for the audio element
   const isEmergency = useAppSelector(selectIsEmergency);
   const dispatch = useAppDispatch();
   const handleOpenEmergency = (isFlag) => {
@@ -35,16 +37,21 @@ const EmergencyPage = () => {
     queryFn: () => {
       return getEmergency();
     },
-    // enabled: !isEmergency, // 모달이 열려있으면 쿼리 비활성화
-    // refetchInterval: isEmergency ? false : 10000, // 모달이 열려있으면 10초마다 쿼리 비활성화
-    enabled: false,
+    enabled: !isEmergency, // 모달이 열려있으면 쿼리 비활성화
+    refetchInterval: isEmergency ? false : 10000, // 모달이 열려있으면 10초마다 쿼리 비활성화
   });
   useEffect(() => {
     if (emergencyData.status === 200) {
       handleOpenEmergency(true);
+      if (audioRef.current) {
+        audioRef.current.play();
+        audioRef.current.loop = true;
+      }
     } else if (emergencyData.status === 204) {
       handleOpenEmergency(false);
       refetchEmergencyData();
+      audioRef.current.pause();
+      audioRef.current.currentTime = 0;
       console.log('없어 다음으로 추적');
     }
   }, [emergencyData.status, isEmergency]);
@@ -54,6 +61,7 @@ const EmergencyPage = () => {
       {emergencyData.data !== undefined && (
         <BoardEmergencyModal open={isEmergency} toggleModal={handleOpenEmergency} data={emergencyData.data} />
       )}
+      <audio ref={audioRef} src={sound} />
     </s.Container>
   );
 };
