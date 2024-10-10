a8a0c727d151334a294a23b5b6667283f51c46b9
1139091SSAFY <iiky2124@gmail.com>
2024-10-10 13:19:51 +0900

    [FE] fix: 이미지 업로드 시 사진 비율 수정
    Former-commit-id: 712ea8c63b32e0d8895d00e5df7c2f8bf599ad85

--- kickcap-user/src/components/Report/ReportMisuseForm.jsx
+++ kickcap-user/src/components/Report/ReportMisuseForm.jsx
@@ -30,7 +30,7 @@ const s = {
     align-items: center;
   `,
   ImageArea: styled.div`
-    width: 70%;
+    width: 80%;
     /* height: fit-content; */
     position: relative;
     margin: 20px auto;
@@ -38,6 +38,7 @@ const s = {
   Image: styled.img`
     width: 100%;
     max-height: 300px;
+    object-fit: contain;
   `,
   ImageBtn: styled.button`
     width: 30px;
